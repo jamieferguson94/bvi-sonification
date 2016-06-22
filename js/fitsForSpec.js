97acae3fbcf56221b232bf27cea5e5687ee7f2f3
adam-avison <adam.avison@manchester.ac.uk>
2016-06-22 16:27:16 +0100

    New Index page

---
+++ js/fitsForSpec.js
@@ -0,0 +1,8 @@
+// needs js/fits.js
+
+
+function setCanvSize(width,height){
+    document.getElementById('ImageCanvas').width = width;
+    document.getElementById('ImageCanvas').height = height;
+
+}
